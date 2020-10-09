fb406a40b0592faada293cee36172c3c77e651a3
nickqweaver <nick.weaver@sendoutcards.com>
2020-10-08 22:26:41 -0600

    Added comment

---
+++ Sources/ReusableSlider/ReusableSlider.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nick Weaver on 10/8/20.
+//
+// ReusableSlider
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, *)
+public struct ReusableSlider: View {
+    public init() {
+        
+    }
+    public var body: some View {
+        Text("REUSABLE SLIDER!")
+    }
+}
